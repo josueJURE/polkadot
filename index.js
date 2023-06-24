49e8f875788ac3868b0dc5f5da10fb45fb797290
josueJURE <josue.jure@gmail.com>
2023-06-24 19:42:37 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const exchangeIcon = document.querySelector(".fa-exchange");
+const plusIcon = document.querySelector(".fa-plus");
+
+exchangeIcon.addEventListener("click", function() {
+    console.log('exchange')
+});
+
+plusIcon.addEventListener("click", function() {
+    console.log('plus icon')
+})
+
+
